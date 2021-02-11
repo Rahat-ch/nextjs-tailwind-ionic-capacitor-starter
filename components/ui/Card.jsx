a104be09c547fc44cd1ffb983648762e2b3cbbdc
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2021-02-11 17:27:40 -0500

    add update

--- components/ui/Card.jsx
+++ components/ui/Card.jsx
@@ -1,9 +1,10 @@
 import classNames from 'classnames';
 
 const Card = ({ children, className, ...props }) => (
-  <div {...props} className={classNames('max-w-xl', className)}>
-    <div className="bg-white shadow-md rounded-b-xl dark:bg-black">{children}</div>
-  </div>
+    <div {...props} className={classNames('max-w-xl', className)}>
+        <h1>Rahat is awesome</h1>
+        <div className="bg-white shadow-md rounded-b-xl dark:bg-black">{children}</div>
+    </div>
 );
 
 export default Card;
